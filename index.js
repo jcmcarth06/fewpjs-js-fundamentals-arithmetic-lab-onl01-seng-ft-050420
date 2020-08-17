c4ccd79ca2f4805af996aef632ccbe56a2546730
Joshua McArthur <jcmcarth06@gmail.com>
2020-08-17 19:58:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 
 // Write your code here
 
-const var1 = 2
-const var2 = 31
+const var1 = 2;
+const var2 = 31;
 
-const multiply = var1 * var2
+const multiply = var1 * var2;
+
+var random = Math.ceil(Math.random() * 10 + 1);
+var mod = 9 % 5;
+var max = Math.max(1,2,3,4,20)
+
+
